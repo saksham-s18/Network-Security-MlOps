086de725cc4f54ff210d1d8b8d81747fd861f748
Saksham-Singh18 <sakshamsingh1876@gmail.com>
2026-02-11 18:48:06 +0530

    pipeline is completed

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 from networksecurity.utils.main_utils.utils import load_object
 from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME,DATA_INGESTION_DATABASE_NAME
@@ -38,6 +39,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+from fastapi.templating import Jinja2Templates
+templates=Jinja2Templates(directory="./templates")
 
 @app.get("/",tags=["authentication"])
 async def index():
@@ -52,6 +55,26 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
+@app.post("/predict")
+async def predict_route(request:Request,file:UploadFile=File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        preprocessor=load_object("final_models/preprocessor.pkl")
+        final_model=load_object("final_models/model.pkl")
+        network_model=NetworkModel(preprocessor=preprocessor,model=final_model)
+        print(df.iloc[0])
+        y_pred=network_model.predict(df)
+        print(y_pred)
+        df['predicted_column']=y_pred
+        df.to_csv('prediction_output/output.csv')
+        table_html=df.to_html(classes='table-striped')
+        return templates.TemplateResponse("table.html",{"request":request,"table":table_html})
+    
+
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
 
 if __name__=="__main__":
     app.run(app,host="localhost",port=8000)
